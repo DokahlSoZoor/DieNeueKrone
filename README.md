6f3a11917163a278a56f82b0f176633cfe6d4716
DokahlSoZoor <manu.hobert@gmail.com>
2018-02-28 17:06:58 +0100

    Articles are now being generated asyncronously in the web app

--- README.md
+++ README.md
@@ -3,12 +3,16 @@ Die Neue Krone
 
 Goal
 ----
-This project aims to develop a fully automated content driven website. The “Neue Krone” is the name of this generative news site where all the articles are created by machine learning models. Once trained the models are put into production generating in regular intervals articles on a wordpress site.
+This project aims to develop a fully automated content driven website. The “Neue Krone” is the name of this generative news site where all the articles are created by machine learning models.
+
+
 
 
 Resources
 ---------
 
-The code is heavily based on this repo and talk:
+This project is heavily based on this talk and the accompanying code:
  - https://github.com/martin-gorner/tensorflow-rnn-shakespeare
  - https://www.youtube.com/watch?v=vq2nnJ4g6N0&t=107m25s
+
+The dataset used for training the model: http://mlg.ucd.ie/datasets/bbc.html
--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START app]
 import logging
 import traceback
-
+from multiprocessing import Process, Queue
 from flask import Flask
 
 import model
 
-
 app = Flask(__name__)
+
+q = Queue()
+p = None
+
+def generate_articles(output_q, amount=5):
+    for title, body in model.generate_articles('checkpoints/rnn_train_1519647475-248000000', amount=amount):
+        output_q.put((title, body))
+        print('article generated')
+
+if __name__ == '__main__':
+
+    p = Process(target=f, args=(q,))
+    p.daemon = True
+    p.start()
+    print(q.get())    # prints "[42, None, 'hello']"
+    p.join()
 
 
 @app.route('/')
@@ -32,8 +32,17 @@ def hello():
 @app.route('/sample')
 def sample():
     """Generate and return an article"""
-    title, body = next(model.generate_articles('checkpoints/rnn_train_1519647475-248000000', amount=1))
-    return """<h1>{}</h1><p>{}</p>""".format(title, body.replace('\n', '<br>'))
+    # title, body = next(model.generate_articles('checkpoints/rnn_train_1519647475-248000000', amount=1))
+    if q.qsize() < 2:
+        global p
+        if p == None or not p.is_alive():
+            p = Process(target=generate_articles, args=(q,))
+            p.daemon = True
+            p.start()
+        return "try again in a moment"
+    else:
+        title, body = q.get()
+        return """<h1>{}</h1><p>{}</p>""".format(title, body.replace('\n', '<br>'))
 
 @app.errorhandler(500)
 def server_error(e):
@@ -49,4 +58,3 @@ if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END app]
